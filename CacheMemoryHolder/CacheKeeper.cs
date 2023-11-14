a147709336ca0a4e7641e0821986b22a3e8270b1
EviatarZilberman <esztbk@gmail.com>
2023-11-14 19:52:16 +0200

    Add project files.

---
+++ CacheMemoryHolder/CacheKeeper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace CacheMemoryHolder
+{
+    public class CacheKeeper
+    {
+        public static IMemoryCache? MemoryCache = null;
+        private static CacheKeeper? instance = null;
+        
+        private CacheKeeper() 
+        {
+        }
+
+        public static CacheKeeper Instance ()
+        {
+            if (instance == null)
+            {
+                instance = new CacheKeeper();
+            }
+            return instance;
+        }
+
+        public async Task CreateMemoryCache<T> (T anyObject, string key, int timeInSeconds)
+        {
+            MemoryCache memoryCache = new MemoryCache(new MemoryCacheOptions());
+            memoryCache.Set<T>(key, anyObject, TimeSpan.FromSeconds(timeInSeconds));
+
+            MemoryCache = memoryCache;
+        }
+
+        /*public async Task GetItemAsync<T> (MemoryCache memoryCache)
+        {
+            //List<T>? items;
+            T items; // anyObject???
+            items = MemoryCache.Get<T>("item");
+            //items?.Add(anyObject);
+
+            MemoryCache?.Set("item", items, TimeSpan.FromSeconds(10));
+        }*/
+    }
+}
---
+++ CacheMemoryHolder/Program.cs
@@ -0,0 +1,73 @@
+﻿using CacheMemoryHolder;
+using Microsoft.Extensions.Caching.Memory;
+
+Person person1 = new Person() { Id = "1", Name = "Eviatar" };
+Person person2 = new Person() { Id = "2", Name = "Tali" };
+Person person3 = new Person() { Id = "3", Name = "Shai" };
+List<Person> list = new List<Person> { person1, person2, person3 };
+
+await CacheKeeper.Instance().CreateMemoryCache(list, "person1", 10);
+/*await Holder.Instance().CreateMemoryCache(person2, "person2", 10);
+await Holder.Instance().CreateMemoryCache(person3, "person3", 10);*/
+
+List<Person> people = null;
+CacheKeeper.MemoryCache?.TryGetValue("person1", out people);
+foreach(var v in people)
+{
+    Console.WriteLine(v.Id);
+}
+
+Thread.Sleep(11000);
+
+CacheKeeper.MemoryCache?.TryGetValue("person1", out people);
+try
+{
+    foreach (var v in people)
+    {
+        Console.WriteLine(v.Id);
+    }
+} catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+/*Person? newPerson1 = null;
+Person? newPerson2 = null;
+Holder.MemoryCache?.TryGetValue("person", out newPerson1);
+Console.WriteLine("Id1:" + newPerson1?.Id);
+Thread.Sleep(11000);
+Holder.MemoryCache?.TryGetValue("person", out newPerson2);
+Console.WriteLine("Id2:" + newPerson2?.Id);*/
+
+Console.ReadKey();
+
+/*
+ using System;
+using System.Runtime.Caching;
+
+class Program
+{
+    static void Main()
+    {
+        // Creating a MemoryCache instance
+        MemoryCache cache = MemoryCache.Default;
+
+        // Adding an item to the cache with a key and value, and specifying cache options (e.g., expiration time)
+        cache.Add("Key1", "Value1", DateTimeOffset.Now.AddMinutes(10));
+
+        // Retrieving a cached item using its key
+        object cachedValue = cache.Get("Key1");
+        if (cachedValue != null)
+        {
+            Console.WriteLine("Cached Value: " + cachedValue.ToString());
+        }
+        else
+        {
+            Console.WriteLine("Key1 not found in the cache.");
+        }
+
+        // Removing an item from the cache using its key
+        cache.Remove("Key1");
+
+        // Checking if an item exists in the cache
+        bool itemExists =
+*/
